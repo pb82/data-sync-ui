c79bb5d5d2920d7692d61852296830dfb241562b
Peter Braun <peter-braun@gmx.net>
2018-07-06 21:22:59 +0200

    remove fh-health

--- server/health/index.js
+++ server/health/index.js
@@ -1,1 +1,66 @@
-export * from "./healthchecks";
+import { database as db } from "../models";
+
+export function runHealthChecks() {
+    // Checks database connectivity by auth attempt
+    function databaseConnectivityCheck () {
+        return db.authenticate()
+    }
+
+    function handleResolve ({label, promise}) {
+        promise = promise.then(() => {
+            return {[label]: true};
+        });
+        return {label, promise};
+    }
+
+    function handleReject ({label, promise}) {
+        return promise.catch(err => {
+            console.error(err);
+            return {[label]: false};
+        })
+    }
+
+    /**
+     * Summarize the results of all checks in to a report of the form of:
+     * {
+   *   ok: <overall status>,
+   *   checks: [
+   *     {check1: <check1 status>},
+   *     {check2: <check2 status>}
+   *   ]
+   * }
+     * @param results
+     * @returns {{ok: boolean, checks: *}}
+     */
+    function summarize (results) {
+        const overallStatus = {
+            ok: false,
+            checks: results
+        };
+
+        overallStatus.ok = results.reduce((acc, result) => {
+            return acc && Object.values(result).reduce((acc, val) => acc && val, true);
+        }, true);
+
+        return overallStatus;
+    }
+
+    /**
+     * Runs all health checks defined in `checks` and returns an array
+     * with the results in the form of:
+     * [
+     *   {check1: <check1 status>},
+     *   {check2: <check2 status>}
+     * ]
+     */
+    function run () {
+        // Add additional health checks to this array
+        const checks = [{
+            label: 'Database Connectivity', promise: databaseConnectivityCheck()
+        }].map(handleResolve).map(handleReject);
+
+        return Promise.all(checks);
+    }
+
+    return run().then(summarize);
+}
