94402df7bb5b3e8c56d2e0fba8ef2e0fb1aa7290
Peter Braun <peter-braun@gmx.net>
2018-07-12 18:05:52 +0200

    add resolver sequelize model

--- server/models/index.js
+++ server/models/index.js
@@ -2,16 +2,26 @@ const { Sequelize } = require("sequelize");
 const { createDatabase, supportsiLike } = require("./database");
 const DataSourceModel = require("./dataSource");
 const SchemaModel = require("./schema");
+const ResolverModel = require("./resolver");
 
 const database = createDatabase();
 const dataSource = DataSourceModel(database, Sequelize);
 const schema = SchemaModel(database, Sequelize);
+const resolver = ResolverModel(database, Sequelize);
+
+resolver.belongsTo(dataSource, {
+    onDelete: "cascade",
+    foreignKey: {
+        allowNull: false
+    }
+});
 
 const sync = () => database.sync({ force: false });
 
 module.exports = {
     database,
     dataSource,
+    resolver,
     sync,
     schema,
     supportsiLike
